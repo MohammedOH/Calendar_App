20f2e581eb77d6afd56e8de7eff0a5f20d38250d
Usama <oszayyan91@hotmail.com>
2019-12-06 23:11:50 +0200

    Show Title on Calender

--- app/src/main/java/com/example/discreteproject/CalenderActivity.java
+++ app/src/main/java/com/example/discreteproject/CalenderActivity.java
@@ -25,6 +25,8 @@ public class CalenderActivity extends AppCompatActivity {
 
         setResultToUI(calenderTable);
 
+        ((TextView)findViewById(R.id.title)).setText(year + "  " + getMonthName(month));
+
     }
 
     private void inflateItems() {
@@ -138,4 +140,23 @@ public class CalenderActivity extends AppCompatActivity {
         }
     }
 
+    /** Get the English name for the month */
+    public String getMonthName(int month) {
+        String monthName = "";
+        switch (month) {
+            case 1: monthName = "January"; break;
+            case 2: monthName = "February"; break;
+            case 3: monthName = "March"; break;
+            case 4: monthName = "April"; break;
+            case 5: monthName = "May"; break;
+            case 6: monthName = "June"; break;
+            case 7: monthName = "July"; break;
+            case 8: monthName = "August"; break;
+            case 9: monthName = "September"; break;
+            case 10: monthName = "October"; break;
+            case 11: monthName = "November"; break;
+            case 12: monthName = "December";
+        }
+        return monthName;
+    }
 }
